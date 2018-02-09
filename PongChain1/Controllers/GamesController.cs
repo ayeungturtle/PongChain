067da22b7e68c82a352e55964f2d2737a69b3019
Andrew Yeung <AYeung@IQInc.local>
2018-02-09 14:42:41 -0500

    odds functioning, date issue fixed

--- PongChain1/Controllers/GamesController.cs
+++ PongChain1/Controllers/GamesController.cs
@@ -51,11 +51,11 @@ namespace PongChain1.Controllers
                 .Where(p => p.Id == id)
                 .FirstOrDefault();
 
-            if(name == null)
+            if (name == null)
             {
                 return NotFound();
             }
-            
+
             var firstName = name.FirstName;
 
             var lastName = name.LastName;
@@ -79,16 +79,125 @@ namespace PongChain1.Controllers
                 .Where(p => p.Id == PlayerId1)
                 .FirstOrDefault();
 
-            var firstName1 = name1.FirstName;
+            var firstName1 = "";
+            var firstName2 = "";
+            if (name1 != null)
+            {
+                firstName1 = name1.FirstName;
+            }
 
             var name2 = db.Players
                 .Where(p => p.Id == PlayerId2)
                 .FirstOrDefault();
 
-            var firstName2 = name2.FirstName;
+            if (name2 != null)
+            {
+                firstName2 = name2.FirstName;
+            }
 
 
             return Ok(firstName1 + ' ' + winCount1 + " - " + winCount2 + " " + firstName2);
+        }
+
+        [HttpGet]
+        [Route("api/GenerateOdds/{PlayerId1}/{PlayerId2}")]
+        public IHttpActionResult GenerateOdds(int PlayerId1, int PlayerId2)
+        {
+            decimal winCount1 = db.Games
+                .Where(g => g.WinnerPlayersId == PlayerId1)
+                .Count();
+
+            decimal loseCount1 = db.Games
+                .Where(g => g.LoserPlayersId == PlayerId1)
+                .Count();
+
+            var name1 = db.Players
+                .Where(p => p.Id == PlayerId1)
+                .FirstOrDefault();
+
+            if (name1 == null)
+            {
+                return NotFound();
+            }
+
+            decimal winCount2 = db.Games
+                .Where(g => g.WinnerPlayersId == PlayerId2)
+                .Count();
+
+            decimal loseCount2 = db.Games
+                .Where(g => g.LoserPlayersId == PlayerId2)
+                .Count();
+
+            var name2 = db.Players
+                .Where(p => p.Id == PlayerId2)
+                .FirstOrDefault();
+
+            if (name2 == null)
+            {
+                return NotFound();
+            }
+
+            var firstName1 = name1.FirstName;
+            var firstName2 = name2.FirstName;
+
+            decimal overAllPercentage1 = winCount1 / (winCount1 + loseCount1);
+            decimal overAllPercentage2 = winCount2 / (winCount2 + loseCount2);
+            //decimal overAllCompare1 = overAllPercentage1 / overAllPercentage2; //How many times bigger is Player 1's win percentage than Player 2's?
+            //decimal overAllCompare2 = overAllPercentage2 / overAllPercentage1; //How many times bigger is Player 2's win percentage than Player 1's?
+
+            decimal h2hWinCount1 = db.Games
+               .Where(g => g.WinnerPlayersId == PlayerId1 && g.LoserPlayersId == PlayerId2)
+               .Count();
+
+            decimal h2hWinCount2 = db.Games
+                .Where(g => g.WinnerPlayersId == PlayerId2 && g.LoserPlayersId == PlayerId1)
+                .Count();
+
+            decimal h2hPercentage1 = .5M;  //fix this later....prevented divide by 0 erro
+            decimal h2hPercentage2 = .5M;
+
+            if (h2hWinCount1 + h2hWinCount2 != 0)
+            {
+                h2hPercentage1 = h2hWinCount1 / (h2hWinCount1 + h2hWinCount2);
+                h2hPercentage2 = h2hWinCount2 / (h2hWinCount1 + h2hWinCount2);
+            }
+            //decimal h2hCompare1 = h2hPercentage1 / h2hPercentage2; //How many times bigger is Player 1's head-to-head percentage than Player 2's?
+            //decimal h2hCompare2 = h2hPercentage2 / h2hPercentage1; //How many times bigger is Player 2's head-to-head percentage than Player 1's?
+
+            decimal averagePercentage1 = (overAllPercentage1 + h2hPercentage1) / 2.0M;
+            decimal averagePercentage2 = (overAllPercentage2 + h2hPercentage2) / 2.0M;
+
+            decimal combinedCompare1 = averagePercentage1 / averagePercentage2;
+            decimal combinedCompare2 = averagePercentage2 / averagePercentage1;
+
+            decimal favoriteCompare = 1.0M;
+            decimal underdogCompare = 1.0M;
+            var favoriteName = "";
+            var underdogName = "";
+            if (combinedCompare1 >= combinedCompare2)
+            {
+                favoriteCompare = combinedCompare1;
+                underdogCompare = combinedCompare2;
+                favoriteName = firstName1;
+                underdogName = firstName2;
+            }
+            else
+            {
+                favoriteCompare = combinedCompare2;
+                underdogCompare = combinedCompare1;
+                favoriteName = firstName2;
+                underdogName = firstName1;
+            }
+
+            decimal oddsMultiplierFavorite = 2 / (favoriteCompare + 1);
+            decimal oddsMultiplierUnderdog = favoriteCompare * oddsMultiplierFavorite;
+            decimal favoritePercentageChance = 50 * oddsMultiplierFavorite;
+            decimal underdogPercentageChance = 50 * oddsMultiplierUnderdog;
+
+            decimal oddsFavorite = 100 * underdogCompare;
+            decimal oddsUnderdog = 100 * favoriteCompare;
+
+            return Ok("Favorite: " + favoriteName + " + $" + oddsFavorite.ToString("#.##") + "   ------ Underdog: " + underdogName + " + $" + oddsUnderdog.ToString("#.##"));
         }
 
         // PUT: api/Games/5
